d4b6b3f14301afd69914c042956293ab1cd52dc3
Ana Ventura <anaventura1811@gmail.com>
2021-10-30 14:13:11 -0300

    style: adiciona estilização de componente messagesList, add sass às dependências

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,6 +1,54 @@
+import styles from './styles.module.scss';
+import logo from '../../assets/logo.svg';
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logo} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar este evento, com certeza vai ser o melhor de todos os tempos! Vamo pra cima!!🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/anaventura1811.png" alt="Ana Ventura"
+              />
+            </div>
+            <span>Ana Ventura</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar este evento, com certeza vai ser o melhor de todos os tempos! Vamo pra cima!!🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/anaventura1811.png" alt="Ana Ventura"
+              />
+            </div>
+            <span>Ana Ventura</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar este evento, com certeza vai ser o melhor de todos os tempos! Vamo pra cima!!🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/anaventura1811.png" alt="Ana Ventura"
+              />
+            </div>
+            <span>Ana Ventura</span>
+          </div>
+        </li>
+        
+      </ul>
+    </div>
   );
 };
 
