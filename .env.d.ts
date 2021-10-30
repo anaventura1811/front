f4f0ef341bbc11eb82eee275fecf3ed3d361ae54
Ana Ventura <anaventura1811@gmail.com>
2021-10-30 15:42:47 -0300

    fix: corrige configuração para variáveis de ambiente, inicia criação de fluxo de autenticação e login

---
+++ .env.d.ts
@@ -0,0 +1,9 @@
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_APP_GITHUB_CLIENT_ID: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
