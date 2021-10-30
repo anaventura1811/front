2156b605deb49bd8c64beb8a81922daa645338e0
Ana Ventura <anaventura1811@gmail.com>
2021-10-30 15:06:25 -0300

    feat: configura chamada à api em node, muda logo e inicia configurações de login com github

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,7 +1,10 @@
 import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc';
+import 'dotenv/config';
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=http://localhost:3000/`;
+
   return (
     <div className={styles.loginBoxContainer}>
       <strong>Entre e compartilhe sua mensagem</strong>
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,52 +1,62 @@
+import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
-import logo from '../../assets/logo.svg';
+import Logo from './DoWhile';
+import { api } from '../../services/api';
+
+type Message = {
+  id: string;
+  text: string;
+  author: {
+    name: string;
+    avatar_url: string;
+  }
+}
 
 export function MessageList() {
+
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last').then((res) => {
+     setIsLoading(true);
+      setMessages(res.data);
+      setIsLoading(false);
+      console.log(messages);
+    });
+    return () => {
+     
+    }
+  }, []);
+
+  // if (isLoading) {
+  //   return 'Carregando...';
+  // }
+
   return (
     <div className={styles.messageListWrapper}>
-      <img src={logo} alt="DoWhile 2021" />
+      <Logo />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar este evento, com certeza vai ser o melhor de todos os tempos! Vamo pra cima!!🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/anaventura1811.png" alt="Ana Ventura"
-              />
-            </div>
-            <span>Ana Ventura</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar este evento, com certeza vai ser o melhor de todos os tempos! Vamo pra cima!!🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/anaventura1811.png" alt="Ana Ventura"
-              />
-            </div>
-            <span>Ana Ventura</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar este evento, com certeza vai ser o melhor de todos os tempos! Vamo pra cima!!🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/anaventura1811.png" alt="Ana Ventura"
-              />
-            </div>
-            <span>Ana Ventura</span>
-          </div>
-        </li>
-        
+
+        { messages.map((message) => {
+          return (
+            <li className={styles.message} key={message.id}>
+              <p className={styles.messageContent}>
+                {message.text}
+              </p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img
+                    src={message.author.avatar_url}
+                    alt={message.author.name}
+                  />
+                </div>
+                <span>{message.author.name}</span>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   );
